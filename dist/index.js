1bf9e446cee5f8b49670a5cbd6ff1f4b89a36f37
myTerminal <team.terminal@gmail.com>
2017-11-06 21:02:02 -0600

    move around scripts

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-"use strict";module.exports={watch:function(t,c,o){o.forEach(function(c){t.start(c)}),console.log("Starting gulp-watch..."),t.watch(c,o)}};
+"use strict";module.exports={watch:function(t,c,o){o.forEach(function(c){return t.start(c)}),console.log("Starting gulp-watch..."),t.watch(c,o)}};
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@
 
 module.exports = {
     watch: function (gulp, files, tasks) {
-        tasks.forEach(function (t) {
-            gulp.start(t);
-        });
+        tasks.forEach(t => gulp.start(t));
 
         console.log('Starting gulp-watch...');
-        
+
         gulp.watch(files, tasks);
     }
 };
